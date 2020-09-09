294e6a07f7c6297e9675a56f354f21f6753e909d
MoonyPadfoot <eurekavixen@gmail.com>
2020-09-09 18:20:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tutornearme/SignInActivity.java
@@ -0,0 +1,90 @@
+package com.example.tutornearme;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SignInActivity extends AppCompatActivity {
+    private final static int LOGIN_REQUEST_CODE = 1717;
+    private List<AuthUI.IdpConfig> providers;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener listener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        init();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(listener);
+    }
+
+    @Override
+    protected void onStop() {
+        if(firebaseAuth != null && listener != null){
+            firebaseAuth.removeAuthStateListener(listener);
+        }
+        super.onStop();
+    }
+
+    private void init(){
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        listener = myFirebaseAuth -> {
+            FirebaseUser user = myFirebaseAuth.getCurrentUser();
+            if(user != null){
+                Toast.makeText(this,"Welcome: " + user.getUid(), Toast.LENGTH_SHORT).show();
+            }else{
+                showLoginLayout();
+            }
+        };
+    }
+
+    private void showLoginLayout() {
+        AuthMethodPickerLayout authMethodPickerLayout = new AuthMethodPickerLayout
+                .Builder(R.layout.activity_sign_in)
+                .setPhoneButtonId(R.id.btn_phone_sign_in)
+                .setGoogleButtonId(R.id.btn_google_sign_in)
+                .build();
+
+        startActivityForResult(AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAuthMethodPickerLayout(authMethodPickerLayout)
+                .setIsSmartLockEnabled(false)
+                .setTheme(R.style.LoginTheme)
+                .setAvailableProviders(providers)
+                .build(), LOGIN_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == LOGIN_REQUEST_CODE){
+            IdpResponse idpResponse = IdpResponse.fromResultIntent(data);
+            if(resultCode == RESULT_OK){
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            }else{
+                Toast.makeText(SignInActivity.this,"[ERROR]: " + idpResponse.getError().getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
